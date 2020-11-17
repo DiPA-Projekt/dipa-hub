b1553b9699131fb10d5141fd9df4505bfb963adb
bock <bock@4soft.de>
2020-11-17 12:13:16 +0100

    refactor: Zeichnen der Milestones, Tasks und X-Achse aus chart.components.ts herausgelöst fix: DIPA-106 Tooltip & Sprungkorrektur

---
+++ frontend/projects/dipa-frontend/src/app/modules/gantt/chart/chart-elements/MilestonesArea.ts
@@ -0,0 +1,163 @@
+import * as d3 from 'd3';
+import {parseSvg} from 'd3-interpolate/src/transform/parse';
+import {IChartElement} from './IChartElement';
+
+export class MilestonesArea implements IChartElement {
+
+  svg;
+  readonly xScale;
+  data: any[];
+  tooltip;
+
+  dateOptions = { year: 'numeric', month: 'numeric', day: 'numeric' };
+
+  elementHeight = 20;
+  elementMargin = 8;
+  elementHeightWithMargin = this.elementHeight + 2 * this.elementMargin;
+
+  elementColor = '#62a9f9';
+
+  constructor(svg: any, xScale: any, data: any[]) {
+    this.svg = svg;
+    this.xScale = xScale;
+    this.data = data;
+
+    this.tooltip = d3.select('figure#chart .tooltip');
+  }
+
+  draw(offset): void {
+
+    const dataGroup = this.svg.select('g.data-group');
+
+    const drag = d3.drag()
+      .on('drag', (event: d3.D3DragEvent<any, any, any>) => {
+
+        // milestones
+        const eventMilestone = dataGroup.select('#milestoneEntry_' + event.subject.id);
+
+        const xTransformValue = parseSvg(eventMilestone.attr('transform')).translateX;
+        const yTransformValue = parseSvg(eventMilestone.attr('transform')).translateY;
+
+        const xValueNew = (xTransformValue + event.dx);
+
+        eventMilestone.attr('transform', 'translate(' + xValueNew + ',' + yTransformValue + ')');
+
+        event.subject.date = this.xScale.invert(xValueNew);
+
+        this.showTooltip(event.subject, event.sourceEvent.layerX, event.sourceEvent.layerY);
+      });
+
+    // milestones
+    const milestone = dataGroup.selectAll('g.milestoneEntry')
+      .data(this.data)
+      .enter()
+      .append('g')
+      .attr('class', 'milestoneEntry')
+      .attr('id', (d) => 'milestoneEntry_' + d.id)
+      .attr('transform', (d, i) => 'translate(' + (offset.left + this.xScale(new Date(d.date))) + ','
+        + (offset.top + this.elementHeightWithMargin * (i % 3) + this.elementHeight / 2) + ')')
+      .call(drag);
+
+    milestone
+      .append('path')
+      .attr('class', 'milestone')
+      .attr('transform', 'scale(1.5 1)')
+      .attr('d', d3.symbol().type(d3.symbolDiamond))
+      .style('fill', this.elementColor)
+      .style('stroke', d3.rgb(this.elementColor).darker());
+
+    milestone
+      .on('mouseover', (event, d) => {
+        this.showTooltip(d, event.layerX, event.layerY);
+      })
+      .on('mouseout', () => {
+        this.tooltip
+          .transition()
+          .duration(500)
+          .style('opacity', 0)
+          .transition()
+          .delay(500)
+          .style('display', 'none');
+      });
+
+    const maxLabelWidth = 40;
+
+    // milestone labels
+    milestone
+      .append('text')
+      .text(d => d.name)
+      .attr('class', 'milestoneLabel')
+      .attr('x', 0)
+      .attr('y', 20)
+      .style('fill', d3.rgb(this.elementColor).darker())
+      .attr('text-anchor', 'middle')
+      .call(this.wrapLabel, maxLabelWidth);
+  }
+
+  redraw(offset): void {
+    // milestones
+    const dataGroup = this.svg.select('g.data-group');
+
+    dataGroup.selectAll('g.milestoneEntry')
+      .attr('transform', (d, i) => 'translate(' + (offset.left + this.xScale(new Date(d.date))) + ','
+        + (offset.top + this.elementHeightWithMargin * (i % 3) + this.elementHeight / 2) + ')');
+  }
+
+  showTooltip(d, x, y): void {
+    this.tooltip
+      .style('top', (y + 15) + 'px')
+      .style('left', (x) + 'px')
+      .style('display', 'block')
+      .attr('font-size', 11)
+      .html(`${d.name}<br>`
+        + `Fällig: ${new Date(d.date).toLocaleDateString('de-DE', this.dateOptions)}<br>`)
+      .transition()
+      .duration(500)
+      .style('opacity', 1);
+  }
+
+  wrapLabel(svgText, maxWidth): void {
+
+    svgText.each(function(): void {
+      const text = d3.select(this);
+      const words = text.text().split(/\s+/);
+
+      let line = [];
+      let lineNumber = 0;
+      const lineHeight = 1.1;
+      const y = text.attr('y');
+
+      let tspan = text
+        .text(null)
+        .append('tspan')
+        .attr('x', 0)
+        .attr('y', y);
+
+      for (const word of words) {
+
+        line.push(word);
+
+        if (tspan.node().getComputedTextLength() > maxWidth) {
+          line.pop();
+          tspan.text(line.join(' '));
+          line = [word];
+
+          tspan = text
+            .append('tspan')
+            .attr('x', 0)
+            .attr('y', y)
+            .attr('dy', ++lineNumber * lineHeight + 'em')
+            .text(word);
+        } else {
+          tspan.text(line.join(' '));
+        }
+      }
+    });
+  }
+
+  getAreaHeight(): number {
+    // for now the milestones are spread over 3 rows
+    return Math.max(this.data.length, 3) * this.elementHeightWithMargin;
+  }
+
+}
---
+++ frontend/projects/dipa-frontend/src/app/modules/gantt/chart/chart-elements/XAxis.ts
@@ -0,0 +1,124 @@
+import * as d3 from 'd3';
+
+export class XAxis {
+
+  svg;
+  readonly xScale;
+
+  svgBbox;
+
+  height = 28;
+
+  constructor(svg: any, xScale: any) {
+    this.svg = svg;
+    this.xScale = xScale;
+    this.svgBbox = this.svg.node().getBBox();
+  }
+
+  draw(): void {
+
+    const xGroup = this.svg.select('g.x-group');
+
+    const viewBoxWidth = this.svgBbox.width;
+
+    // x-axis header background
+    xGroup
+      .append('rect')
+      .attr('class', 'headerX')
+      .attr('x', 0)
+      .attr('y', 0)
+      .attr('width', (viewBoxWidth))
+      .attr('height', (this.height));
+
+    // x-axis labels
+    xGroup.selectAll('text')
+      .data(this.xScale.ticks())
+      .enter()
+      .append('text')
+      .attr('class', 'xAxisLabel')
+      .text(d => this.formatDate(d))
+      .attr('x', d => this.xScale(d) + 4)
+      .attr('y', 18);
+
+    // current date indicator
+    xGroup
+      .append('line')
+      .attr('class', 'currentDate')
+      .attr('x1', 0)
+      .attr('x2', Math.min(Math.max(this.xScale(new Date()), 0), this.xScale.range()[1]))
+      .attr('y1', this.height)
+      .attr('y2', this.height);
+
+    this.drawVerticalGridLines();
+  }
+
+  redraw(): void {
+
+    const xGroup = this.svg.select('g.x-group');
+
+    // x-axis labels
+    xGroup.selectAll('text.xAxisLabel').remove();
+    xGroup.selectAll('text')
+      .data(this.xScale.ticks())
+      .enter()
+      .append('text')
+      .attr('class', 'xAxisLabel')
+      .text(d => this.formatDate(d))
+      .attr('x', d => this.xScale(d) + 4)
+      .attr('y', 18);
+
+    // current date indicator
+    xGroup.select('line.currentDate')
+      .attr('x2', Math.min(Math.max(this.xScale(new Date()), 0), this.xScale.range()[1]));
+
+    this.redrawVerticalGridLines();
+  }
+
+  resize(newSize): void {
+    const xGroup = this.svg.select('g.x-group');
+
+    // x-axis header background
+    xGroup.select('rect.headerX')
+      .attr('width', (newSize));
+  }
+
+  private drawVerticalGridLines(): void {
+    const xGroup = this.svg.select('g.x-group');
+
+    const viewBoxHeight = this.svgBbox.height;
+
+    // vertical grid lines
+    xGroup.selectAll('line.xGridLines')
+      .data(this.xScale.ticks())
+      .enter()
+      .append('line')
+      .attr('class', 'xGridLines')
+      .attr('x1', d => this.xScale(d))
+      .attr('x2', d => this.xScale(d))
+      .attr('y1', 0)
+      .attr('y2', viewBoxHeight)
+      .style('stroke', '#eee');
+  }
+
+  redrawVerticalGridLines(): void {
+    const xGroup = this.svg.select('g.x-group');
+
+    // vertical grid lines
+    xGroup.selectAll('line.xGridLines').remove();
+    this.drawVerticalGridLines();
+  }
+
+  // Define filter conditions
+  formatDate(date): any {
+    return (d3.timeSecond(date) < date ? d3.timeFormat('.%L')
+        : d3.timeMinute(date) < date ? d3.timeFormat(':%S')
+          : d3.timeHour(date) < date ? d3.timeFormat('%H:%M')
+            : d3.timeDay(date) < date ? d3.timeFormat('%H:%M')
+              : d3.timeMonth(date) < date ? (d3.timeWeek(date) < date ? d3.timeFormat('%a, %d.') : d3.timeFormat('%d. %b'))
+                : d3.timeYear(date) < date ? d3.timeFormat('%b')
+                  : d3.timeFormat('%Y')
+    )
+    (date);
+  }
+
+}
