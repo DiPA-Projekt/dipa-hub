5657ea45166b9797f7e06182f7c01fd063f873c9
Johannes Becker <johannes.becker@4soft.de>
2020-11-25 20:02:21 +0100

    feat: DIPA-76: add support for multiple tenants. (#23)
    * feat: DIPA-76: add support for multiple tenants.
    
    Updated README.md

--- server/src/test/java/online/dipa/hub/persistence/repositories/ProjectTypeRepositoryTest.java
+++ server/src/test/java/online/dipa/hub/persistence/repositories/ProjectTypeRepositoryTest.java
@@ -2,11 +2,13 @@ package online.dipa.hub.persistence.repositories;
 
 import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import online.dipa.hub.persistence.entities.ProjectTypeEntity;
+import online.dipa.hub.tenancy.CurrentTenantContextHolder;
 
 @SpringBootTest
 class ProjectTypeRepositoryTest {
@@ -15,13 +17,28 @@ class ProjectTypeRepositoryTest {
     private ProjectTypeRepository projectTypeRepository;
 
     @Test
-    void allDataInitializedCorrectly() {
-        assertThat(projectTypeRepository.findAll()).hasSize(3)
+    void allBaDataInitializedCorrectly() {
+        CurrentTenantContextHolder.setTenantId("ba");
+        assertThat(projectTypeRepository.findAll()).hasSize(2)
                                                    .extracting(ProjectTypeEntity::getId, ProjectTypeEntity::getName,
                                                            ProjectTypeEntity::isDefaultType)
-                                                   .containsExactly(tuple(1L, "Serveraustausch", false),
-                                                           tuple(2L, "Softwareneuentwicklung", false),
-                                                           tuple(3L, "Beschaffung", true));
+                                                   .containsExactly(tuple(1L, "Serveraustausch", true),
+                                                           tuple(3L, "Beschaffung", false));
+    }
+
+    @Test
+    void allItzbundDataInitializedCorrectly() {
+        CurrentTenantContextHolder.setTenantId("itzbund");
+        assertThat(projectTypeRepository.findAll()).hasSize(2)
+                                                   .extracting(ProjectTypeEntity::getId, ProjectTypeEntity::getName,
+                                                           ProjectTypeEntity::isDefaultType)
+                                                   .containsExactly(tuple(2L, "Softwareneuentwicklung", true),
+                                                           tuple(3L, "Beschaffung", false));
+    }
+
+    @AfterEach
+    void deleteTentantId() {
+        CurrentTenantContextHolder.clearTenantId();
     }
 
 }
