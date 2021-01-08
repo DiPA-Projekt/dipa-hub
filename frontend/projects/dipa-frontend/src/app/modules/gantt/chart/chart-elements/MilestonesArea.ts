3a6dd6f69e5d9ad6003f2a5f817f2d8d656d5c61
Nhan Nhu Ngoc Nguyen <nhan.nhu.ngoc.nguyen@msg.group>
2021-01-08 16:16:17 +0100

    style: delete console.log

--- frontend/projects/dipa-frontend/src/app/modules/gantt/chart/chart-elements/MilestonesArea.ts
+++ frontend/projects/dipa-frontend/src/app/modules/gantt/chart/chart-elements/MilestonesArea.ts
@@ -158,7 +158,6 @@ export class MilestonesArea implements IChartElement {
       if (this.showMenu) {
 
         milestoneIcon.on('click', (event, d) => {
-          console.log('click')
 
           if (d.id !== this.selectedMilestoneId) {
 
