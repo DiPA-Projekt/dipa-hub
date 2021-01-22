d6d1cfc908ed50aa8ef0d9065f4077927a549f0c
Martin Oppitz <github@martinoppitz.com>
2021-01-22 04:45:28 +0100

    chore: config ts app config for eslint

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -8,7 +8,7 @@ module.exports = {
     es6: true,
     node: true,
   },
-  extends: ['eslint:recommended'],
+  extends: ["eslint:recommended"],
   overrides: [
     /**
      * - https://www.npmjs.com/package/@typescript-eslint/eslint-plugin
@@ -16,51 +16,51 @@ module.exports = {
      */
     {
       extends: [
-        'plugin:@typescript-eslint/recommended',
-        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
       ],
-      files: ['**/*.ts', '**/*.tsx'],
+      files: ["**/*.ts", "**/*.tsx"],
       parserOptions: {
         ecmaVersion: 2018,
-        project: './tsconfig.json',
-        extraFileExtensions: ['.vue'],
-        sourceType: 'module',
+        project: "./projects/dipa-frontend/tsconfig.app.json",
+        extraFileExtensions: [".vue"],
+        sourceType: "module",
       },
-      plugins: ['@typescript-eslint'],
+      plugins: ["@typescript-eslint"],
       rules: {
-        '@typescript-eslint/ban-types': 'off',
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-empty-function': 'off',
-        '@typescript-eslint/no-explicit-any': 'off',
-        '@typescript-eslint/no-unused-vars': 'off',
-        '@typescript-eslint/no-var-requires': 'off',
+        "@typescript-eslint/ban-types": "off",
+        "@typescript-eslint/explicit-function-return-type": "off",
+        "@typescript-eslint/no-empty-function": "off",
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-unused-vars": "off",
+        "@typescript-eslint/no-var-requires": "off",
       },
     },
     /**
      * - https://www.npmjs.com/package/eslint-plugin-html
      */
     {
-      files: ['**/*.html'],
-      plugins: ['html'],
+      files: ["**/*.html"],
+      plugins: ["html"],
     },
     /**
      * - https://www.npmjs.com/package/eslint-plugin-json
      */
     {
-      extends: ['plugin:json/recommended'],
-      files: ['**/*.json'],
-      plugins: ['json'],
+      extends: ["plugin:json/recommended"],
+      files: ["**/*.json"],
+      plugins: ["json"],
     },
     {
       env: {
         mocha: true,
       },
-      files: ['tests/**/*', '**/*.spec.*', '**/*.test.*'],
+      files: ["tests/**/*", "**/*.spec.*", "**/*.test.*"],
     },
   ],
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   rules: {
-    'no-empty': 'off',
-    'no-prototype-builtins': 'off',
+    "no-empty": "off",
+    "no-prototype-builtins": "off",
   },
 };
