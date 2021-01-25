789bbf9c89397c80dbc449ac16f7746decc5c7a0
deleonio <6279703+deleonio@users.noreply.github.com>
2021-01-25 03:25:26 +0100

    Dry run : setup lightweight testing (#125)
    * chore: add mocha test setup instead of ng test
    
    * fix: package json script for mocha:ci

---
+++ frontend/projects/dipa-frontend/src/app/modules/gantt/chart/chart-elements/XAxis.test.ts
@@ -0,0 +1,36 @@
+import { expect } from 'chai';
+import * as d3 from 'd3';
+
+import { XAxis } from './XAxis';
+
+const createContext = (): {
+  element: unknown,
+  svg: unknown,
+} => {
+  const ELEMENT = document.createElement('div');
+  const SVG = d3
+    .select(ELEMENT)
+    .append('svg')
+    .append('g')
+    .attr('class', 'x-group');
+  return {
+    element: ELEMENT,
+    svg_ SVG,
+  };
+};
+
+describe('XAxis', () => {
+  let xAxis: XAxis;
+  let context: unknown;
+
+  beforeEach(() => {
+    context = createContext();
+    try {
+      xAxis = new XAxis(context.svg, context.element, d3.scaleTime());
+    } catch (error) {
+      expect(true).to.equal(true);
+    }
+  });
+
+  it('Versuch die XAxis zu instanziieren', () => {});
+});
