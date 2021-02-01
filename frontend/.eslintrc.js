769966d4fc994cece0dfd5893d91f24d6f17e8fe
Martin Oppitz <github@martinoppitz.com>
2021-02-01 10:40:56 +0100

    chore: format config files

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -2,24 +2,24 @@ module.exports = {
   root: true,
   overrides: [
     {
-      files: ["*.ts"],
+      files: ['*.ts'],
       parserOptions: {
-        project: ["tsconfig.json", "e2e/tsconfig.json"],
+        project: ['tsconfig.json', 'e2e/tsconfig.json'],
         tsconfigRootDir: __dirname,
         createDefaultProgram: true,
       },
       extends: [
-        "plugin:@angular-eslint/ng-cli-compat",
-        "plugin:@angular-eslint/ng-cli-compat--formatting-add-on",
-        "plugin:@angular-eslint/template/process-inline-templates",
-        "plugin:@typescript-eslint/recommended",
-        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        'plugin:@angular-eslint/ng-cli-compat',
+        'plugin:@angular-eslint/ng-cli-compat--formatting-add-on',
+        'plugin:@angular-eslint/template/process-inline-templates',
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
       ],
       rules: {},
     },
     {
-      files: ["*.html"],
-      extends: ["plugin:@angular-eslint/template/recommended"],
+      files: ['*.html'],
+      extends: ['plugin:@angular-eslint/template/recommended'],
       rules: {},
     },
   ],
