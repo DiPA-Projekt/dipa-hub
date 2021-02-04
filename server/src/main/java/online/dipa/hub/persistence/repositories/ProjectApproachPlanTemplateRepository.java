2da581a11a287853b052b6c0858c6bc3d045df10
Nhan Nhu Ngoc Nguyen <nhan.nhu.ngoc.nguyen@msg.group>
2021-02-04 15:10:14 +0100

    fix: delete unused repository

--- server/src/main/java/online/dipa/hub/persistence/repositories/ProjectApproachPlanTemplateRepository.java
+++
@@ -1,7 +0,0 @@
-package online.dipa.hub.persistence.repositories;
-
-import online.dipa.hub.persistence.entities.PlanTemplateEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProjectApproachPlanTemplateRepository extends JpaRepository<PlanTemplateEntity, Long> {
-}
