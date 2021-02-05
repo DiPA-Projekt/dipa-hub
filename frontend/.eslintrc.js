6fb34866d887c2f27deeefcdf4a14de84667add5
deleonio <6279703+deleonio@users.noreply.github.com>
2021-02-05 06:46:31 +0100

    chore: edit eslint configuration (#169)
    * chore: edit eslint configuration
    
    * fix: auto fix eslint error

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -15,7 +15,14 @@ module.exports = {
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
       ],
-      rules: {},
+      rules: {
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/explicit-function-return-type': 'off',
+        '@typescript-eslint/no-empty-function': 'off',
+        '@typescript-eslint/no-explicit-any': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+        '@typescript-eslint/no-var-requires': 'off',
+      },
     },
     {
       files: ['*.html'],
