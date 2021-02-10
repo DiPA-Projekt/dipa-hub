f2588e5918d4bc401905c909fd34b3f2ba21ca67
Nhan Nhu Ngoc Nguyen <nhan.nhu.ngoc.nguyen@msg.group>
2021-02-10 09:29:03 +0100

    refactor: split into several controllers, services

---
+++ server/src/main/java/online/dipa/hub/server/rest/MilestoneController.java
@@ -0,0 +1,41 @@
+package online.dipa.hub.server.rest;
+
+import online.dipa.hub.api.rest.MilestonesApi;
+
+import online.dipa.hub.api.model.*;
+import online.dipa.hub.services.MilestoneService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestApiController
+public class MilestoneController implements MilestonesApi{
+
+    @Autowired
+    MilestoneService milestoneService;
+    
+    @Override
+    public ResponseEntity<List<Milestone>> getMilestonesForTimeline(final Long timelineId) {
+        final List<Milestone> milestoneList = milestoneService.getMilestonesForTimeline(timelineId);
+        return ResponseEntity.ok(milestoneList);
+    }
+
+    
+    @Override
+    public ResponseEntity<Void> updateMilestoneData(final Long timelineId, final Long milestoneId, final Milestone milestone ) {
+        if (Optional.ofNullable(milestone.getStatus()).isPresent()) {
+            milestoneService.updateMilestoneStatus(timelineId, milestoneId, milestone.getStatus());
+        }
+        return ResponseEntity.noContent().build();
+    }
+
+    @Override
+    public ResponseEntity<List<DownloadFile>> getFilesForMilestone(final Long timelineId, final Long milestoneId) {
+        final List<DownloadFile> attachedFilesList = milestoneService.getFilesForMilestone(timelineId, milestoneId);
+        return ResponseEntity.ok(attachedFilesList);
+    }
+
+    
+}
