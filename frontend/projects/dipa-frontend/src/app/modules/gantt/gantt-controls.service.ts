96544a654a2ba33bb96becc1bfe4c163c24e9221
bock <bock@4soft.de>
2021-02-20 00:04:59 +0100

    feat: DIPA-251 removed save button for project data (saving on blur instead)

--- frontend/projects/dipa-frontend/src/app/modules/gantt/gantt-controls.service.ts
+++ frontend/projects/dipa-frontend/src/app/modules/gantt/gantt-controls.service.ts
@@ -5,20 +5,10 @@ import { BehaviorSubject, Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class GanttControlsService {
-  // not used yet
-  private isFullscreen = new BehaviorSubject<boolean>(false);
-  isFullscreen$ = this.isFullscreen.asObservable();
-
   private periodStartDate = new BehaviorSubject<Date>(new Date(2020, 0, 1));
   private periodEndDate = new BehaviorSubject<Date>(new Date(2020, 11, 31));
 
   private viewType = new BehaviorSubject<string>(null);
-
-  constructor() {}
-
-  setIsFullscreen(isFullscreen: boolean): void {
-    this.isFullscreen.next(isFullscreen);
-  }
 
   getPeriodStartDate(): Observable<Date> {
     return this.periodStartDate;
