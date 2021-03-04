9c95964729b821e8c21a1737db4601a33e391eb4
Nhan Nhu Ngoc Nguyen <nhan.nhu.ngoc.nguyen@msg.group>
2021-03-04 16:57:43 +0100

    feat: DIPA-291 save projectApproach into database

--- server/src/main/java/online/dipa/hub/services/ProjectApproachService.java
+++ server/src/main/java/online/dipa/hub/services/ProjectApproachService.java
@@ -1,6 +1,7 @@
 package online.dipa.hub.services;
 
 import online.dipa.hub.api.model.ProjectApproach;
+import online.dipa.hub.persistence.entities.ProjectApproachEntity;
 import online.dipa.hub.persistence.repositories.ProjectApproachRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 @Transactional
@@ -25,6 +28,14 @@ public class ProjectApproachService {
 
         return projectApproachRepository.findAll().stream()
                 .map(p -> conversionService.convert(p, ProjectApproach.class)).collect(Collectors.toList());
+    }
+
+    public ProjectApproachEntity getProjectApproachFromRepo (final Long projectApproachId) {
+
+        return projectApproachRepository.findAll().stream()
+                .filter(p -> p.getId().equals(projectApproachId))
+                .findFirst().orElseThrow(() -> new EntityNotFoundException(
+                        String.format("Project Approach with id: %1$s not found.", projectApproachId)));
     }
 
     
