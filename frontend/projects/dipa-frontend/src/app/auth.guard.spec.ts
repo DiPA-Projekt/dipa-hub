44a39b40f54bc6ba6083baa794a0b4834bc9ce7f
Dennis Bock <bock@4soft.de>
2021-03-05 10:43:59 +0100

    feat: DIPA-274 restrict access for schedules configuration to pmo role (#222)
    fix: use dateChange event instead of change in matDatepicker

--- frontend/projects/dipa-frontend/src/app/auth.guard.spec.ts
+++ frontend/projects/dipa-frontend/src/app/auth.guard.spec.ts
@@ -1,12 +1,21 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenav, MatSidenavContainer, MatSidenavContent } from '@angular/material/sidenav';
+import { NavMenuListItemComponent } from './shared/nav-menu-list-item/nav-menu-list-item.component';
+import { MatNavList } from '@angular/material/list';
 
 describe('AuthGuard', () => {
   let guard: AuthGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      declarations: [MatSidenavContainer, MatSidenav, MatSidenavContent, NavMenuListItemComponent, MatNavList],
+      imports: [HttpClientTestingModule, MatMenuModule, OAuthModule.forRoot()],
+    });
     guard = TestBed.inject(AuthGuard);
   });
 
--- frontend/projects/dipa-frontend/src/app/authentication.service.spec.ts
+++ frontend/projects/dipa-frontend/src/app/authentication.service.spec.ts
@@ -1,12 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AuthenticationService } from './authentication.service';
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 
 describe('AuthenticationService', () => {
   let service: AuthenticationService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, OAuthModule.forRoot()],
+    });
     service = TestBed.inject(AuthenticationService);
   });
 
