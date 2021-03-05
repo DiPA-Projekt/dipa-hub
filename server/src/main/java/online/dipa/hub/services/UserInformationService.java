df9cc34efe167e8c998683702a1610b9ed758cfc
bock <bock@4soft.de>
2021-03-05 23:53:31 +0100

    removed unused imports

--- server/src/main/java/online/dipa/hub/services/UserInformationService.java
+++ server/src/main/java/online/dipa/hub/services/UserInformationService.java
@@ -2,20 +2,13 @@ package online.dipa.hub.services;
 
 import online.dipa.hub.api.model.User;
 import org.keycloak.adapters.OidcKeycloakAccount;
-import org.keycloak.adapters.springsecurity.account.SimpleKeycloakAccount;
 import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
 import org.keycloak.representations.AccessToken;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
-import java.security.Principal;
 import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 
 @Service
