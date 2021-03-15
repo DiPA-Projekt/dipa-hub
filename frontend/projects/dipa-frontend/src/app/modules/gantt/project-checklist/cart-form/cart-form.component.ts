5b15ea741025cb045dbf43d9be0f03794cf9bfdf
Dennis Bock <bock@4soft.de>
2021-03-15 10:56:06 +0100

    feat: DIPA-354 cancel edit input text field by clicking escape button (#227)
    * feat: DIPA-354 cancel edit input text field by clicking escape button
    
    * fix: 354 input field was set to null if pressed esc and previous value was empty

--- frontend/projects/dipa-frontend/src/app/modules/gantt/project-checklist/cart-form/cart-form.component.ts
+++ frontend/projects/dipa-frontend/src/app/modules/gantt/project-checklist/cart-form/cart-form.component.ts
@@ -8,21 +8,45 @@ import { ELBEshoppingCartResult } from 'dipa-api-client';
   styleUrls: ['../project-task-form/project-task-form.component.scss'],
 })
 export class CartFormComponent implements OnInit {
-  @Input() formData;
-  @Input() statusList;
-  @Output() dataChanged = new EventEmitter();
+  @Input() public formData;
+  @Input() public statusList;
+  @Output() public dataChanged = new EventEmitter();
 
-  formGroup: FormGroup;
+  public formGroup: FormGroup;
 
-  constructor(public controlContainer: ControlContainer, parent: FormGroupDirective, private fb: FormBuilder) {
+  public constructor(public controlContainer: ControlContainer, parent: FormGroupDirective, private fb: FormBuilder) {
     this.formGroup = parent.control;
   }
 
-  ngOnInit(): any {
+  public ngOnInit(): void {
     this.setReactiveForm(this.formData);
   }
 
-  setReactiveForm(data: ELBEshoppingCartResult[]): void {
+  public get cartsArray(): FormArray {
+    return this.formGroup.get(['results', 'data']) as FormArray;
+  }
+
+  public addCart(): void {
+    this.cartsArray.push(this.emptyCart);
+  }
+
+  public deleteCart(index: number): void {
+    this.cartsArray.removeAt(index);
+    this.dataChanged.emit();
+  }
+
+  public onFocus(event: FocusEvent, path: (string | number)[]): void {
+    const valueInput = event.target as HTMLInputElement;
+    valueInput.setAttribute('data-value', this.formGroup.get(path).value || '');
+  }
+
+  public onEscape(event: KeyboardEvent, path: (string | number)[]): void {
+    const valueInput = event.target as HTMLInputElement;
+    valueInput.value = valueInput.getAttribute('data-value');
+    this.formGroup.get(path).setValue(valueInput.value);
+  }
+
+  private setReactiveForm(data: ELBEshoppingCartResult[]): void {
     for (const cart of data) {
       this.cartsArray.push(
         this.fb.group({
@@ -33,28 +57,15 @@ export class CartFormComponent implements OnInit {
         })
       );
     }
-    this.formGroup.get('results').get('type').setValue('TYPE_ELBE_SC');
+    this.formGroup.get(['results', 'type']).setValue('TYPE_ELBE_SC');
   }
 
-  get emptyCart(): FormGroup {
+  private get emptyCart(): FormGroup {
     return this.fb.group({
       resultType: 'TYPE_ELBE_SC',
       shoppingCartNumber: '',
       shoppingCartContent: '',
       status: null,
     });
-  }
-
-  get cartsArray(): FormArray {
-    return this.formGroup.get('results').get('data') as FormArray;
-  }
-
-  addCart(): void {
-    this.cartsArray.push(this.emptyCart);
-  }
-
-  deleteCart(index: number): void {
-    this.cartsArray.removeAt(index);
-    this.dataChanged.emit();
   }
 }
