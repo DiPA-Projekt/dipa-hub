99e08fad9ebe8257acf3c1d08547a1b3b3ed1b5a
MWindsheimer <github-maik.windsheimer@itzbund.de>
2021-05-28 09:32:05 +0200

    login tests edited and completed

--- frontend/tests/cucumber/step-definitions/login_steps.js
+++ frontend/tests/cucumber/step-definitions/login_steps.js
@@ -2,30 +2,49 @@ const { client } = require('nightwatch-api');
 const { Given, When, Then } = require('@cucumber/cucumber');
 
 Given(/^Ich starte die App$/i, async () => {
-  await client.url('http://localhost:4200');
+  await client.url('https://develop.dipa.online/');
   await client.waitForElementVisible('body', 1000);
 });
 
 // TODO: Erkennungsmerkmale für Elemente auslagern in pageObjects
 When(/^Ich melde mich als "([^"]*)" an$/, username => {
-  return client
-    .assert.visible('input[id="username"]')
-    .setValue('input[id="username"]', username)
-    .assert.visible('input[id="password"]')
-    .setValue('input[id="password"]', 12345)
-    .click('input[id=kc-login]');
+  return login(username, "12345");
 });
 
-When(/^Ich melde mich als "{string}" mit "{string}" an$/, (username, password) => {
-  return client
-    .assert.visible('input[id="username"]')
-    .setValue('input[id="username"]', username)
-    .assert.visible('input[id="password"]')
-    .setValue('input[id="password"]', password)
-    .click('input[id=kc-login]');
+When(/^Ich melde mich als "([^"]*)" mit "([^"]*)" an$/, (username, password) => {
+  return login(username, password);
+});
+
+When(/^Ich melde mich ohne Credentials an$/, () => {
+  return login("", "");
 });
 
 Then(/^sollte die Startseite zu sehen sein$/, () => {
   return client
-    .waitForElementVisible('//*[text()="account_circle"]');
+    .waitForElementVisible('app-root')
+    .assert.containsText("mat-nav-list", "Eine Reise durchs Projekt");
+//  .waitForElementVisible('//*[text()="account_circle"]');
 });
+
+Then (/^Wird der Login wegen falscher Credentials abgewiesen$/, () => {
+  return client
+    .waitForElementVisible('span[id="input-error"]')
+    .assert.containsText('span[id="input-error"]', 'Invalid username or password');
+});
+
+Then(/^sollte ich den Text "([^"]*)" sehen$/, errorText => {
+  return client
+    .waitForElementVisible('span[id="input-error"]')
+    .assert.containsText('span[id="input-error"]', errorText);
+});
+
+
+// functions section
+function login (username, password) {
+  return client
+  .assert.visible('input[id="username"]')
+  .setValue('input[id="username"]', username)
+  .assert.visible('input[id="password"]')
+  .setValue('input[id="password"]', password)
+  .click('input[id=kc-login]');
+};
