2e3f3936d1cabd37ff0e42a24bd1ec8dfc49b4b5
Tansu Akbay <t.dursun@live.de>
2021-06-02 15:36:37 +0200

    Start "Unsere Termine" und Ordnerstruktur

--- frontend/tests/cucumber/step-definitions/assert_steps.js
+++ frontend/tests/cucumber/step-definitions/assert_steps.js
@@ -1,3 +1,16 @@
 const { client } = require('nightwatch-api');
-const { Given, When, Then } = require('@cucumber/cucumber');
+const { Then } = require('@cucumber/cucumber');
+const fs = require('fs')
+// import assert from 'assert/strict';
 
+
+Then(/^sollte die Datei "([^"]*)" unter dem Pfad "([^"]*)" existieren$/, (fileName, path) => {
+  const pathToFile = path + '/' + fileName
+  if (!fs.existsSync(pathToFile)) {
+    return client
+      .assert.ok(false, 'Die Datei "' + pathToFile + '" existiert nicht')
+  }
+  // vorheriger Versuch mit
+  //https://nodejs.org/api/assert.html#assert_assert_ok_value_message
+  //assert.ok(fs.existsSync(pathToFile), 'Datei ' + pathToFile + ' existiert nicht')
+});
--- frontend/tests/cucumber/step-definitions/click_steps.js
+++ frontend/tests/cucumber/step-definitions/click_steps.js
@@ -1,8 +1,11 @@
 const { client } = require('nightwatch-api');
-const { Given, When, Then } = require('@cucumber/cucumber');
+const { When } = require('@cucumber/cucumber');
 
 // Nicht überprüft, da es ein Beispiel ist
-When(/^Ich klicke auf den Button "([^"]*)" $/, title => {
-  return client
-    .click('button[title="' + title + '"]')
+When(/^Ich klicke auf den Button "([^"]*)"$/, title => {
+  return client.click('button[title="' + title + '"]')
+});
+
+When(/^Ich klicke auf den Text "([^"]*)"$/, text => {
+  return client.click('xpath', '*//*[contains(text(), "' + text + '")]')
 });
