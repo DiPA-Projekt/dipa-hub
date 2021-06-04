564b70300ccf2921d001838e79e34a75bbf91b5d
Tansu Akbay <t.dursun@live.de>
2021-06-04 10:44:08 +0200

    Step für das sehen eines Textes verschoben und angepasst

--- frontend/tests/cucumber/step-definitions/assert_steps.js
+++ frontend/tests/cucumber/step-definitions/assert_steps.js
@@ -1,16 +1,18 @@
 const { client } = require('nightwatch-api');
 const { Then } = require('@cucumber/cucumber');
-const fs = require('fs')
+const fs = require('fs');
 // import assert from 'assert/strict';
 
-
 Then(/^sollte die Datei "([^"]*)" unter dem Pfad "([^"]*)" existieren$/, (fileName, path) => {
-  const pathToFile = path + '/' + fileName
+  const pathToFile = path + '/' + fileName;
   if (!fs.existsSync(pathToFile)) {
-    return client
-      .assert.ok(false, 'Die Datei "' + pathToFile + '" existiert nicht')
+    return client.assert.ok(false, 'Die Datei "' + pathToFile + '" existiert nicht');
   }
   // vorheriger Versuch mit
   //https://nodejs.org/api/assert.html#assert_assert_ok_value_message
   //assert.ok(fs.existsSync(pathToFile), 'Datei ' + pathToFile + ' existiert nicht')
+});
+
+Then(/^sollte ich den Text "([^"]*)" sehen$/, (text) => {
+  return client.waitForElementVisible('xpath', '*//*[contains(text(), "' + text + '")]')
 });
