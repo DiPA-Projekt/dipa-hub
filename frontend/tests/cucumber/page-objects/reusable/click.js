236daf95abed4a6e92ef79bfcd5e1c922aee4aff
Tansu Akbay <t.dursun@live.de>
2021-06-09 11:23:28 +0200

    Zeitachse Checks, klickeAufText umbenannt in clickOnText, clickOnButton hinzugefügt

--- frontend/tests/cucumber/page-objects/reusable/click.js
+++ frontend/tests/cucumber/page-objects/reusable/click.js
@@ -1,11 +1,18 @@
 const { client } = require('nightwatch-api');
 
-const klickeAufText = function (text) {
+const clickOnText = function (text) {
   return client
     .waitForElementVisible('xpath', '//*[contains(text(), "' + text + '")]', 5000)
     .click('xpath', '//*[contains(text(), "' + text + '")]');
 };
 
+const clickOnButton = function (buttonName) {
+  return client
+    .waitForElementVisible('xpath', '//button/span[contains(text(), "' + buttonName + '")]', 5000)
+    .click('xpath', '//button/span[contains(text(), "' + buttonName + '")]');
+};
+
 module.exports = {
-  klickeAufText: klickeAufText,
+  clickOnText: clickOnText,
+  clickOnButton: clickOnButton,
 };
