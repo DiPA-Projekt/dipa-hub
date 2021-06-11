7e959132ec2093c13fff4d1e7690b4c455dc6a9d
Nhan Nguyen <42439713+irenenguyen3095@users.noreply.github.com>
2021-06-12 00:35:57 +0200

    fix: DIPA-425, DIPA-422 (#340)
    Co-authored-by: nguyen <nhan.nhu.ngoc.nguyen@msg.group>

--- server/src/main/resources/db/changelog/db.changelog-v0.13.0-data.sql
+++ server/src/main/resources/db/changelog/db.changelog-v0.13.0-data.sql
@@ -7,3 +7,13 @@ VALUES (10, 'Kolibri PMO', 'itzbund', '5d06a955-5e30-4f0e-bf85-2eb73ba4a32a')
 --changeset id:insert-user-kolibri-team
 INSERT INTO app_user (id, name, tenant_id, keycloak_id)
 VALUES (11, 'Kolibri Team', 'itzbund', '21ce19b5-cc3b-4990-b94f-c02999a05648')
+
+--changeset id:update-project-task-05-form-entry-participants-result-template-responsible-person-key context:itzbund
+UPDATE project_task_form_field
+SET key = 'responsible_person'
+WHERE id = 43
+
+--changeset id:update-project-task-05-form-entry-participants-result-all-fields-responsible-person-key context:itzbund
+UPDATE project_task_form_field
+SET key = 'responsible_person'
+WHERE key = 'responsiblePerson'
