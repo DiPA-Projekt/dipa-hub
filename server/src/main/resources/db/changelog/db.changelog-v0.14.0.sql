a006ff871429ec007ba88f25258990e9162d6cb8
nguyen <nhan.nhu.ngoc.nguyen@msg.group>
2021-06-16 12:25:06 +0200

    fix: add delete cascade to constraints

---
+++ server/src/main/resources/db/changelog/db.changelog-v0.14.0.sql
@@ -0,0 +1,23 @@
+--liquibase formatted sql
+
+-- changeset id:delete_FKUserIdProjectRoleConnection
+ALTER TABLE user_project_role_connection
+DROP CONSTRAINT FKUserIdProjectRoleConnection
+
+-- changeset id:update_FKUserIdProjectRoleConnection
+ALTER TABLE user_project_role_connection
+ADD CONSTRAINT FKUserIdProjectRoleConnection
+FOREIGN KEY (user_id)
+REFERENCES app_user(id)
+ON DELETE CASCADE
+
+-- changeset id:delete_FKOrganisationRoleUserIdConnection
+ALTER TABLE user_organisation_role_connection
+DROP CONSTRAINT FKOrganisationRoleUserIdConnection
+
+-- changeset id:update_FKOrganisationRoleUserIdConnection
+ALTER TABLE user_organisation_role_connection
+ADD CONSTRAINT FKOrganisationRoleUserIdConnection
+FOREIGN KEY (user_id)
+REFERENCES app_user(id)
+ON DELETE CASCADE
