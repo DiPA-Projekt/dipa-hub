0434190effce6786c96478f3185eafb9f74fdfd4
Dennis Bock <bock@4soft.de>
2021-06-17 13:39:59 +0200

    fix: DIPA-370 migrate existing project task form fields (hint -> label) (#354)

--- server/src/main/resources/db/changelog/db.changelog-v0.14.0-data.sql
+++ server/src/main/resources/db/changelog/db.changelog-v0.14.0-data.sql
@@ -5,3 +5,8 @@ DELETE FROM app_user WHERE tenant_id = 'itzbund'
 
 -- changeset id:delete_user_from_itzbund context:itzbund
 DELETE FROM app_user WHERE tenant_id = 'ba'
+
+--changeset id:update-project-task-form-fields-hint
+UPDATE project_task_form_field
+SET label = hint, hint = null
+WHERE hint is not null
