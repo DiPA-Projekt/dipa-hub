7a44af136d8a96b3871093575fcfd0a0a76f9fbd
Nhan Nguyen <42439713+irenenguyen3095@users.noreply.github.com>
2021-07-13 11:37:15 +0200

    refactor: jqpl for repositories (#408)
    * refactor: add queries into repositories
    
    * refactor: improve some queries and methods
    
    * fix: delete unused imports
    
    * refactor: add tests for repositories
    
    * refactor: add test cases for PlanTemplateRepositoryTest
    
    * refactor: change findById to getById
    
    * fix: delete unused import
    
    * prettier check
    
    Co-authored-by: nguyen <nhan.nhu.ngoc.nguyen@msg.group>

---
+++ server/src/test/java/online/dipa/hub/persistence/repositories/ProjectTaskTemplateRepositoryTest.java
@@ -0,0 +1,50 @@
+ package online.dipa.hub.persistence.repositories;
+
+ import static org.assertj.core.api.BDDAssertions.then;
+
+ import java.util.Optional;
+
+ import org.junit.jupiter.api.Nested;
+ import org.junit.jupiter.api.Test;
+ import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.boot.test.context.SpringBootTest;
+
+ import online.dipa.hub.persistence.entities.ProjectTaskTemplateEntity;
+ import online.dipa.hub.tenancy.CurrentTenantContextHolder;
+
+ @SpringBootTest
+ class ProjectTaskTemplateRepositoryTest {
+
+     @Autowired
+     private ProjectTaskTemplateRepository projectTaskTemplateRepository;
+
+     @Nested
+     class FindByMaster {
+
+         @Test
+         void should_return_when_master_template_exists() {
+             CurrentTenantContextHolder.setTenantId("itzbund");
+
+             // WHEN
+             final Optional<ProjectTaskTemplateEntity> projectTaskTemplate = projectTaskTemplateRepository.findByMaster();
+
+             // THEN
+             then(projectTaskTemplate).get()
+                               .returns(1L, ProjectTaskTemplateEntity::getId)
+                               .returns(null, ProjectTaskTemplateEntity::getProject);
+         }
+
+         @Test
+         void should_not_return_when_master_template_not_exists() {
+             CurrentTenantContextHolder.setTenantId("ba");
+
+             // WHEN
+             final Optional<ProjectTaskTemplateEntity> projectTaskTemplate = projectTaskTemplateRepository.findByMaster();
+
+             // THEN
+             then(projectTaskTemplate).isEmpty();
+         }
+
+     }
+
+ }
