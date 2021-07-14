a153f6c9908124a42b25b6234fcd6219a74f5032
MWindsheimer <github-maik.windsheimer@itzbund.de>
2021-07-14 10:40:41 +0200

    test: new page objects and steps for creating a project

---
+++ frontend/tests/cucumber/step-definitions/Projekt anlegen/projekt_anlegen_steps.js
@@ -0,0 +1,13 @@
+const { client } = require('nightwatch-api');
+const { Given, When, Then } = require('@cucumber/cucumber');
+const login = require('../../page-objects/login');
+
+// to do: implement, if needed
+When(/^Ich starte den Dialog für ein neues Projekt$/, () => {
+  
+});
+
+// to do: implement, if needed
+Then(/^Sind die Felder korrekt vorbelegt$/, () => {
+
+});
