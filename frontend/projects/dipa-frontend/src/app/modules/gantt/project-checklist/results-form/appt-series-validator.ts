34f47a587380acd11d929c28a08c5af85c153707
bock <bock@4soft.de>
2021-08-05 13:23:37 +0200

    fix: fixed formGroup sometimes not set and validator can handle old data format now

--- frontend/projects/dipa-frontend/src/app/modules/gantt/project-checklist/results-form/appt-series-validator.ts
+++ frontend/projects/dipa-frontend/src/app/modules/gantt/project-checklist/results-form/appt-series-validator.ts
@@ -3,14 +3,34 @@ import { FormArray, FormGroup, ValidatorFn } from '@angular/forms';
 
 @Injectable({ providedIn: 'root' })
 export class AppointmentSeriesValidator {
+  private static allFieldsSet(formFieldsArray: FormArray): boolean {
+    const serie = formFieldsArray.controls.find((y) => y.get('key').value === 'serie')?.get('value');
+    const startTime = formFieldsArray.controls.find((y) => y.get('key').value === 'startTime')?.get('value');
+    const duration = formFieldsArray.controls.find((y) => y.get('key').value === 'duration')?.get('value');
+    const startDate = formFieldsArray.controls.find((y) => y.get('key').value === 'startDate')?.get('value');
+    const endDate = formFieldsArray.controls.find((y) => y.get('key').value === 'endDate')?.get('value');
+    const appointment = formFieldsArray.controls.find((y) => y.get('key').value === 'appointment')?.get('value');
+    const participants = formFieldsArray.controls.find((y) => y.get('key').value === 'participants')?.get('value');
+
+    return (
+      serie?.value !== '' &&
+      startTime?.value !== '' &&
+      duration?.value !== '' &&
+      startDate?.value !== '' &&
+      endDate?.value !== '' &&
+      appointment?.value !== '' &&
+      participants?.value !== ''
+    );
+  }
+
   public validRruleAndStatusSet(): ValidatorFn {
     return (formGroup: FormGroup) => {
       const formFieldsArray = formGroup.get('formFields') as FormArray;
       const allFieldsSet = AppointmentSeriesValidator.allFieldsSet(formFieldsArray);
 
-      const status = formFieldsArray.controls.find((y) => y.get('key').value === 'status').get('value');
+      const status = formFieldsArray.controls.find((y) => y.get('key').value === 'status')?.get('value');
 
-      return status.value === '' && !allFieldsSet ? { invalidAppointmentSeries: true } : null;
+      return status?.value === '' && !allFieldsSet ? { invalidAppointmentSeries: true } : null;
     };
   }
 
@@ -19,11 +39,11 @@ export class AppointmentSeriesValidator {
       const formFieldsArray = formGroup.get('formFields') as FormArray;
       const allFieldsSet = AppointmentSeriesValidator.allFieldsSet(formFieldsArray);
 
-      const status = formFieldsArray.controls.find((y) => y.get('key').value === 'status').get('value');
+      const status = formFieldsArray.controls.find((y) => y.get('key').value === 'status')?.get('value');
 
-      if (allFieldsSet && status.disabled) {
+      if (allFieldsSet && status?.disabled) {
         status.enable();
-      } else if (!allFieldsSet && !status.disabled) {
+      } else if (!allFieldsSet && !status?.disabled) {
         status.disable();
       }
 
@@ -35,30 +55,10 @@ export class AppointmentSeriesValidator {
     return (formGroup: FormGroup) => {
       const formFieldsArray = formGroup.get('formFields') as FormArray;
 
-      const startDate = formFieldsArray.controls.find((y) => y.get('key').value === 'startDate').get('value');
-      const endDate = formFieldsArray.controls.find((y) => y.get('key').value === 'endDate').get('value');
+      const startDate = formFieldsArray.controls.find((y) => y.get('key').value === 'startDate')?.get('value');
+      const endDate = formFieldsArray.controls.find((y) => y.get('key').value === 'endDate')?.get('value');
 
-      return new Date(startDate.value) > new Date(endDate.value) ? { startBeforeEnd: true } : null;
+      return new Date(startDate?.value) > new Date(endDate?.value) ? { startBeforeEnd: true } : null;
     };
-  }
-
-  private static allFieldsSet(formFieldsArray: FormArray): boolean {
-    const serie = formFieldsArray.controls.find((y) => y.get('key').value === 'serie').get('value');
-    const startTime = formFieldsArray.controls.find((y) => y.get('key').value === 'startTime').get('value');
-    const duration = formFieldsArray.controls.find((y) => y.get('key').value === 'duration').get('value');
-    const startDate = formFieldsArray.controls.find((y) => y.get('key').value === 'startDate').get('value');
-    const endDate = formFieldsArray.controls.find((y) => y.get('key').value === 'endDate').get('value');
-    const appointment = formFieldsArray.controls.find((y) => y.get('key').value === 'appointment').get('value');
-    const participants = formFieldsArray.controls.find((y) => y.get('key').value === 'participants').get('value');
-
-    return (
-      serie.value !== '' &&
-      startTime.value !== '' &&
-      duration.value !== '' &&
-      startDate.value !== '' &&
-      endDate.value !== '' &&
-      appointment.value !== '' &&
-      participants.value !== ''
-    );
   }
 }
