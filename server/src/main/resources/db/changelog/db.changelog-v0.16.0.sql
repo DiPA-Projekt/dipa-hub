ab628fee7ba743c6325c01572ace25e5280e8641
nguyen <nhan.nhu.ngoc.nguyen@msg.group>
2021-08-08 17:05:57 +0200

    fix: store renaming scripts to new db file

--- server/src/main/resources/db/changelog/db.changelog-v0.16.0.sql
+++ server/src/main/resources/db/changelog/db.changelog-v0.16.0.sql
@@ -83,15 +83,3 @@ CREATE TABLE IF NOT EXISTS public.event
         ON UPDATE CASCADE
         ON DELETE CASCADE
 )
-
--- changeset id:rename-table-event-template-to-project-event-template
-ALTER TABLE event_template
-RENAME TO project_event_template
-
--- changeset id:rename-table-event-to-project-event
-ALTER TABLE event
-RENAME TO project_event
-
--- changeset id:rename-column-event-template-to-project-event-template
-ALTER TABLE project_event
-RENAME event_template_id TO project_event_template_id
