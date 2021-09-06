5efb2f14629045aeb41f4c881d3bb20436fbd72a
Dennis Bock <bock@4soft.de>
2021-09-06 16:20:32 +0200

    fix: fix Code Smell

--- server/src/main/java/online/dipa/hub/server/rest/DownloadFileController.java
+++ server/src/main/java/online/dipa/hub/server/rest/DownloadFileController.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 
 @RestApiController
 public class DownloadFileController implements FilesApi {
