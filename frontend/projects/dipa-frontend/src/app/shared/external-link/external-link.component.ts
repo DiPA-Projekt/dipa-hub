f68097d1cab1c59bca79c84b1b210a40403f72ba
bock <bock@4soft.de>
2021-09-20 17:22:41 +0200

    feat: DIPA-499 add invalid link dialog for favorite links moved link validity check into invalid-link-check directive

--- frontend/projects/dipa-frontend/src/app/shared/external-link/external-link.component.ts
+++ frontend/projects/dipa-frontend/src/app/shared/external-link/external-link.component.ts
@@ -1,7 +1,4 @@
 import { Component, Input } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { InvalidLinkDialogComponent } from '../invalid-link-dialog/invalid-link-dialog.component';
-import Utils from '../utils';
 
 @Component({
   selector: 'app-external-link',
@@ -10,18 +7,4 @@ import Utils from '../utils';
 export class ExternalLinkComponent {
   @Input() public url: string;
   @Input() public title: string;
-
-  public constructor(public dialog: MatDialog) {}
-
-  public checkValidUrl(url: string): boolean {
-    return Utils.isValidUrl(url);
-  }
-
-  public openInvalidUrlModal(urlTitle: string): void {
-    this.dialog.open(InvalidLinkDialogComponent, {
-      data: {
-        title: urlTitle,
-      },
-    });
-  }
 }
