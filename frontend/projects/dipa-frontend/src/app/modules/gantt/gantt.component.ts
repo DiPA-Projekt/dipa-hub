2b4b1a0e94cac0d36cff270e4b42e5a9b701cd05
bock <bock@4soft.de>
2021-09-21 16:35:00 +0200

    feat: DIPA-502 show important project information at the top of side menu feat: made side menu toggleable

--- frontend/projects/dipa-frontend/src/app/modules/gantt/gantt.component.ts
+++ frontend/projects/dipa-frontend/src/app/modules/gantt/gantt.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-gantt',
   templateUrl: './gantt.component.html',
   styleUrls: ['./gantt.component.scss'],
 })
-export class GanttComponent {}
+export class GanttComponent {
+  public mobileQuery: MediaQueryList;
+
+  private readonly mobileQueryListener: () => void;
+
+  public constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this.mobileQueryListener);
+  }
+}
