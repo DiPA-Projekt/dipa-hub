fbb5139f0f3d6856815b65c95ba8053fdc259189
bock <bock@4soft.de>
2021-10-06 17:15:30 +0200

    fix: fixed Code Smells

--- frontend/projects/dipa-frontend/src/app/modules/configuration/recurring-events/recurring-events.component.ts
+++ frontend/projects/dipa-frontend/src/app/modules/configuration/recurring-events/recurring-events.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-recurring-events',
   templateUrl: './recurring-events.component.html',
 })
-export class RecurringEventsComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit(): void {}
-}
+export class RecurringEventsComponent {}
