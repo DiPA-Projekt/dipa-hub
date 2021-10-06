bb726c45318d6ea786df16af5e6a201048ec039b
bock <bock@4soft.de>
2021-10-06 16:56:36 +0200

    feat: DIPA-453 changed pmo menu structure

---
+++ frontend/projects/dipa-frontend/src/app/modules/configuration/configuration.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { NavItem } from '../../nav-item';
+
+@Component({
+  selector: 'app-configuration',
+  templateUrl: './configuration.component.html',
+  styleUrls: ['./configuration.component.scss'],
+})
+export class ConfigurationComponent implements OnInit {
+  public navMenuItems: NavItem[] = [];
+
+  public ngOnInit(): void {
+    this.setSideNavMenu();
+  }
+
+  private setSideNavMenu(): void {
+    this.navMenuItems = [
+      {
+        name: 'Zeitplankonfiguration',
+        icon: 'date_range',
+        route: 'configurations/schedules',
+      },
+      {
+        name: 'Wiederkehrende Aufgaben',
+        icon: 'change_circle',
+        route: 'configurations/recurring_events',
+      },
+    ];
+  }
+}
---
+++ frontend/projects/dipa-frontend/src/app/modules/configuration/recurring-events/recurring-events.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recurring-events',
+  templateUrl: './recurring-events.component.html',
+})
+export class RecurringEventsComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
